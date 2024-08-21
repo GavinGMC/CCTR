123dc49e41dab941287df6e15be4e8534172af02
Jeremy Jackson <jeremy.jackson1321@gmail.com>
2024-08-21 13:44:04 -0400

    adds comments

--- ELO_algo.py
+++ ELO_algo.py
@@ -1,9 +1,12 @@
-ra = 1600
-rb = 1800
-gamedif = 4
+#ra is the winning player
+ra = 1800
+#rb is the losing player
+rb = 2400
+gamedif = 6
 
 
-def newELO(ra,rb):
+# Calculates Elo difference over one game
+def ELOdif(ra,rb):
     ea = 1/(1+10**((rb-ra)/400))
     eb = 1/(1+10**((ra-rb)/400))
     Ra= ra+(16*(1-ea))
@@ -11,12 +14,14 @@ def newELO(ra,rb):
     difa = Ra-ra
     difb = Rb-rb
     return difa, difb
+# Scales the elo difference by the gamedif
 def rating(ra,rb):
-    difa, difb = newELO(ra,rb)
+    difa, difb = ELOdif(ra,rb)
     ra = ra + gamedif*difa
     rb = rb + gamedif*difb
     return ra,rb
 
 
 ra,rb = rating(ra,rb)
-print (ra,rb)
+print (ra)
+print (rb)
